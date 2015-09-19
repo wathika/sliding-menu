91e6c684c54ccb7cb1384a035b5a37be8874d6c8
wathika <vcwathika@gmail.com>
2015-09-19 14:58:33 -0700

    pushed everything back when close is clicked

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var main = function() {
         $('.menu').animate({
             left: '0px'
             }, 200);
-        });
 
-    $('.body').animate({
-        left: '285px'
+
+        $('.body').animate({
+            left: '285px'
+            }, 200);
+    });
+
+    /* Then push them back */
+    $('.icon-close').click(function() {
+        $('.menu').animate({
+          left: "-285px"
         }, 200);
+
+        $('body').animate({
+          left: "0px"
+        }, 200);
+    });
 
 };
 
